e4027d97fd2919575316fab65722197bcc131bc1
Motocarota <motocarota@gmail.com>
2016-09-26 01:03:24 +0200

    concluso update del server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
-import Server from 'socket.io';
+import Server from 'socket.io'
 
-export default function startServer( ) {
-	const io = new Server( ).attach( 8090 );
+export default function startServer( store ) {
 
+	const io = new Server( ).attach( 8090 )
+
+	//We are now publishing the whole state to everyone whenever any changes occur. 
+	//This may end up causing a lot of data transfer. 
 	store.subscribe(
 		( ) => io.emit( 'state', store.getState( ).toJS( ) )
-	);
+	)
 
 	io.on( 'connection', socket => {
-		socket.emit( 'state', store.getState( ).toJS( ) );
-		socket.on('action', store.dispatch.bind(store));
-	} );
+		//Sending the whole state when the user connects to the appllication 
+		socket.emit( 'state', store.getState( ).toJS( ) )
+
+		//Our clients emit 'action' events that we feed directly into our Redux store
+		socket.on( 'action', store.dispatch.bind( store ) )
+	} )
 }
